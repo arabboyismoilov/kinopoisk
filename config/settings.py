beb19195c7a3abff2fab0faf0a2de4dd7434fea2
Arabboy Ismoilov <arabboyismoilov27@gmail.com>
2026-08-22 11:16:27 +0500

    .env config

--- config/settings.py
+++ config/settings.py
@@ -16,6 +16,7 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 env = environ.Env()
+env.read_env(BASE_DIR / '.env')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.1/howto/deployment/checklist/
